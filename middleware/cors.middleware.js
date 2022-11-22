737fd777a712904de00a43040fb48b1f3bac1f9a
tatiankris <tatiankris@mail.ru>
2022-11-22 23:04:14 +0300

    refactor cors

--- middleware/cors.middleware.js
+++ middleware/cors.middleware.js
@@ -1,7 +1,7 @@
 function cors(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Methods", "GET, PUT, PATCH, POST, DELETE");
-    res.header("Access-Control-Allow-Headers",  "Content-Type, Authorization");
+    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization, X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Date, X-Api-Version");
     next();
 }
 
